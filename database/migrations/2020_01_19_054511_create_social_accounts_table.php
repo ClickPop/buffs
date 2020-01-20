f254be8c2d6e53e053ea659b65dc15630e66326f
Sean Metzgar <sean.metzgar@gmail.com>
2020-01-19 23:36:48 -0500

    Hopefully fixing issue

--- database/migrations/2020_01_19_054511_create_social_accounts_table.php
+++ database/migrations/2020_01_19_054511_create_social_accounts_table.php
@@ -18,10 +18,10 @@ class CreateSocialAccountsTable extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('platform_id')->unsigned();
             $table->string('platform_user_id');
-            $table->string('token')->nullable();
-            $table->string('tokenSecret')->nullable();
-            $table->string('refreshToken')->nullable();
-            $table->timestamp('expires')->nullable();
+            $table->string('token')->nullable()->default(null);
+            $table->string('tokenSecret')->nullable()->default(null);
+            $table->string('refreshToken')->nullable()->default(null);
+            $table->timestamp('expires')->nullable()->default(null);
             $table->timestamps();
 
 	        $table->foreign('user_id')
