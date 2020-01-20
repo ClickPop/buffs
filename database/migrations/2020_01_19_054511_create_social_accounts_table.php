efa763180a3c32ca79efdd289b710f5dfcf8a489
Sean Metzgar <sean.metzgar@gmail.com>
2020-01-19 22:46:51 -0500

    🌱 Update social_accounts migration to prep for tokens/refresh tokens

--- database/migrations/2020_01_19_054511_create_social_accounts_table.php
+++ database/migrations/2020_01_19_054511_create_social_accounts_table.php
@@ -18,6 +18,10 @@ class CreateSocialAccountsTable extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('platform_id')->unsigned();
             $table->string('platform_user_id');
+            $table->string('token');
+            $table->string('tokenSecret');
+            $table->string('refreshToken');
+            $table->timestamp('expires');
             $table->timestamps();
 
 	        $table->foreign('user_id')
