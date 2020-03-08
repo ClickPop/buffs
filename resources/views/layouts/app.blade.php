61095cdbf35a246508ee87a9b5a90f5e7613b8b6
Sean Metzgar <sean.metzgar@gmail.com>
2020-03-08 01:23:03 -0500

    Add Dashboard and Bootstrap Icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,64 @@
-@include('dropins.core.head', ['bodyClass' => 'bg-app'])
+@include('dropins.core.head')
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow py-3">
-    <div class="container">
-        @include('dropins.components.logo-app')
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>
+<div class="app-wrapper has-sidebar">
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3><img class="logo" src="{{ asset('images/brand/buffs_logo.svg') }}" alt="BUFFS"></h3>
+            <strong>BS</strong>
         </div>
-    </div>
-</nav>
 
-<main role="main" class="py-3">
-    @yield('content')
-</main>
+        <ul class="list-unstyled components">
+            <li class="active">
+                <a href="{{ route('dashboard') }}">
+                    @svg('icons/columns-gutters')
+                    Dashboard
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    @svg('icons/wifi')
+                    Streams
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.leaderboards') }}">
+                    @svg('icons/kanban')
+                    Leaderboards
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    @svg('icons/eye')
+                    Referrals
+                </a>
+            </li>
+            <li>
+                <a href="#adminSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    @svg('icons/shield-lock')
+                    Admin
+                </a>
+                <ul class="collapse list-unstyled" id="adminSubmenu">
+                    <li>
+                        <a href="#">Users</a>
+                    </li>
+                    <li>
+                        <a href="#">Streams</a>
+                    </li>
+                    <li>
+                        <a href="{{route('leaderboards.index')}}">Leaderboards</a>
+                    </li>
+                    <li>
+                        <a href="#">Referrals</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+    </nav>
+
+    <main role="main" class="app-content py-3">
+        @yield('content')
+    </main>
+</div>
 
 @include('dropins.core.foot')
-
