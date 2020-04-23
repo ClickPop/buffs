fb9accd5d2ca2be798eaef1773bb9bc71d241696
Sean Metzgar <sean.metzgar@gmail.com>
2020-04-23 00:27:59 -0400

    Prep for disable preview data on dashboard

--- resources/views/dropins/components/leaderboard.blade.php
+++ resources/views/dropins/components/leaderboard.blade.php
@@ -1,37 +1,40 @@
 @php
-    $preview = (isset($preview) && $preview === true) ? "preview" : "";
-    $route = Route::currentRouteName();
-    $referralCounts = [];
-    $wizards = [
-      'Gandalf',
-      'Merlin',
-      'Dumbledore',
-      'Hermione Granger',
-      'Sabrina',
-      'Prospero',
-      'Saruman',
-      'Voldemort',
-      'Elminster',
-      'Tim',
-      'Harry Dresden',
-      'Orwen',
-      'Glinda',
-      'Morgana Le Fay',
-      'Kiki'
+  $route = Route::currentRouteName();
+  $wizardReferrals = [];
+  $wizards = [
+    'Gandalf',
+    'Merlin',
+    'Dumbledore',
+    'Hermione Granger',
+    'Sabrina',
+    'Prospero',
+    'Saruman',
+    'Voldemort',
+    'Elminster',
+    'Tim',
+    'Harry Dresden',
+    'Orwen',
+    'Glinda',
+    'Morgana Le Fay',
+    'Kiki'
+  ];
+  shuffle($wizards);
+  
+  $wizard_num = 0;
+  while(count($wizardReferrals) < 10) {
+    $tempItem = (object)[
+      "referrer" => $wizards[$wizard_num],
+      "count" => 1
     ];
-    shuffle($wizards);
-    
-    $wizard_num = 0;
-    while(count($referralCounts) < 10) {
-      $tempItem = (object)[
-        "referrer" => $wizards[$wizard_num],
-        "count" => 1
-      ];
-      array_push($referralCounts, $tempItem);
-        $wizard_num++;
-    }
+    array_push($wizardReferrals, $tempItem);
+      $wizard_num++;
+  }
+
+  if (isset($preview) && $preview === true) {
+    $previewClass = "preview";
+  } else { $preview = false; }
 @endphp
-<div class="leaderboard-wrapper theme-{{ $leaderboard->theme }} {{ $preview }}"> <!-- Set leaderboard theme in class -->
+<div class="leaderboard-wrapper theme-{{ $leaderboard->theme }} {{ $previewClass ?? '' }}"> <!-- Set leaderboard theme in class -->
   @isset($leaderboard)
   <div class="leaderboard">
     <div class="leaderboard__container">
@@ -42,30 +45,19 @@
       <!-- LOOP -->
       @if (is_array($referrals))
       @for ($i = 0; $i < 10; $i++)
-        <div class="leaderboard__row" style="display: {{ ($route === 'dashboard' && $i >= $leaderboard->length) || ($route !== 'dashboard' && ($i >= $leaderboard->length || $i >= count($referrals))) ? "none;" : "" }}">
+        <div class="leaderboard__row" style="{{ ($route === 'dashboard' && $i >= $leaderboard->length) || ($route !== 'dashboard' && ($i >= $leaderboard->length || $i >= count($referrals))) ? "display: none;" : "" }}">
           @if ($i < count($referrals))
             <div>{{ $referrals[$i]->referrer }}</div>
             <div>{{ $referrals[$i]->count }}</div>
-          @elseif ($route === 'dashboard')
-            <div>{{ $referralCounts[$i]->referrer }}</div>
-            <div>{{ $referralCounts[$i]->count }}</div>
+          @elseif ($route === 'dashboard' && $preview)
+            <div>{{ $wizardReferrals[$i]->referrer }}</div>
+            <div>{{ $wizardReferrals[$i]->count }}</div>
           @else
             <div></div>
             <div></div>
           @endif
         </div>
       @endfor
-      {{-- @elseif(
-      is_array($referrals) && 
-      count($referrals) < 1 && 
-      $route === 'dashboard'
-      )
-        @foreach($referralCounts as $referral)
-          <div class="leaderboard__row" style="display: {{ $loop->index >= $leaderboard->length ? "none;" : "" }}">
-            <div>{{ $referral->referrer }}</div>
-            <div>{{ $referral->count }}</div>
-          </div>
-        @endforeach --}}
       @endif
       <!-- END LOOP -->
     </div>
@@ -75,5 +67,5 @@
 <script type="text/javascript">
   var channel = '<?php echo $user->username; ?>';
   var route = '<?php echo $route; ?>';
-  var wizards = '<?php echo json_encode($referralCounts); ?>';
+  var wizards = '<?php echo json_encode($wizardReferrals); ?>';
 </script>
