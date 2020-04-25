a5732705eb13b06723576c8881be42ad321aad6a
datboi_fourtwenty <rescus1221@gmail.com>
2020-04-25 02:24:45 -0400

    fixed chatbot blade

--- resources/views/dropins/components/chatbot.blade.php
+++ resources/views/dropins/components/chatbot.blade.php
@@ -1,4 +1,4 @@
 <p id="bot-action-statement">
-  {{ $chatbot->message->bot->joined ? 'The bot is in your channel' : 'The bot isn\'t in your channel yet' }}
+  {{ $chatbot->message->data->joined ? 'The bot is in your channel' : 'The bot isn\'t in your channel yet' }}
 </p>
-<button id="bot-action-button" class="btn {{ $chatbot->message->bot->joined ? 'btn-danger' : 'btn-primary' }}">{{ $chatbot->message->bot->joined ? 'Part' : 'Join' }}</button>
+<button id="bot-action-button" class="btn {{ $chatbot->message->data->joined ? 'btn-danger' : 'btn-primary' }}">{{ $chatbot->message->data->joined ? 'Part' : 'Join' }}</button>
